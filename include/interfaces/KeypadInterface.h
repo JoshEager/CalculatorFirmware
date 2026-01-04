2022fec8e6edea03736225e25a2f58a963a90b5e
JoshEager <josheagr07@gmail.com>
2026-01-04 18:05:38 -0500

    added docs for KeypadInterface

--- include/interfaces/KeypadInterface.h
+++ include/interfaces/KeypadInterface.h
@@ -5,6 +5,19 @@
 #include <array>
 #include "config.h"
 
+/**
+ * 
+ * @brief A class (with an instance defined below) for interfacing with the keypad 
+ * @author Josh Eager
+ * @date 1-4-26
+ * 
+ * To key keys from the keypad, simply read from keypad->key_queue using xQueueRecieve().
+ * If a key has been pressed, then it will be placed on the queue. To modify anything 
+ * about the keypad_interface object itself, check src/interfaces/KeypadInterface.cpp
+ * to see how it is initialized. You can change the keymapping there.
+ * 
+ */
+
 class KeypadInterface 
 {
 private:
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include "config.h"
+#include "config.h" // for the baud rate of serial
 #include "interfaces/KeypadInterface.h"
 
 void setup()
