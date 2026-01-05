fac6ee95413765457fd2ee5dccd014790cea32bc
JoshEager <josheagr07@gmail.com>
2026-01-04 22:17:09 -0500

    fixed config system

--- include/pinout.h
+++ include/pinout.h
@@ -9,22 +9,19 @@
  */
 
 
- // Pinouts for the lcd screen
- #define MOSI_PIN 11
- #define MISO_PIN 13
- #define SCK_PIN 12
- #define LCD_CS_PIN 10
+// PINOUTS FOR THE LCD SCREEN ARE IN PLATFORMIO.INI IN ORDER TO PASS THEM TO TFT_eSPI!!! PLEASE CHECK THERE 
 
- // Pinouts for the matrix keypad
- #define ROW1_PIN 1
- #define ROW2_PIN 4
- #define ROW3_PIN 5
- #define ROW4_PIN 6
- #define ROW5_PIN 7
- #define ROW6_PIN 9
- #define ROW7_PIN 14
 
- #define COL1_PIN 21
- #define COL2_PIN 18
- #define COL3_PIN 17
- #define COL4_PIN 20
+// Pinouts for the matrix keypad
+#define ROW1_PIN 1
+#define ROW2_PIN 4
+#define ROW3_PIN 5
+#define ROW4_PIN 6
+#define ROW5_PIN 7
+#define ROW6_PIN 9
+#define ROW7_PIN 14
+
+#define COL1_PIN 21
+#define COL2_PIN 18
+#define COL3_PIN 17
+#define COL4_PIN 20
