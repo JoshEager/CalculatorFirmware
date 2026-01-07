6e8f5f85e777cb720b904839717f6f60d4245e7b
JoshEager <josheagr07@gmail.com>
2026-01-07 14:05:29 -0500

    unpinned KeypadInterface from core 1

--- src/interfaces/KeypadInterface.cpp
+++ src/interfaces/KeypadInterface.cpp
@@ -19,7 +19,7 @@ namespace KeypadInterface
 
     void _KeypadInterface::pollingTask()
     {
-        PRINTDBG("Entered pollingTask()\n");
+        PRINTDBG("Entered pollingTask()");
         while (1)
         {
             if (kpd->getKey())
@@ -54,14 +54,13 @@ namespace KeypadInterface
         // Start the polling task. The polling task puts the keys it reads into the key press queue.
         // Note: The task entry function must be a class method and not a normal instance method
         // because of compatibily with the c api for freeRTOS.
-        xTaskCreatePinnedToCore(
+        xTaskCreate(
             pollingTaskEntry,
             "Keypad Driver - polling",
             4096,
             (void *)this,
             1,
-            &polling_task_handle,
-            1);
+            &polling_task_handle);
     }
 
     static char key_mappings[ROWS][COLS] = {
